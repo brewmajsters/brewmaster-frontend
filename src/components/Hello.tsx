80432466c539c0f09b54c0784d95513f89994c06
Mmmattt123 <matej.janeba@gmail.com>
2019-10-22 11:43:52 +0200

    added webpack and TS

---
+++ src/components/Hello.tsx
@@ -0,0 +1,13 @@
+// @ts-ignore
+import React from 'react';
+import {Component} from 'react';
+
+
+
+export class Hello extends Component{
+    render() {
+        return <h1>Hello Brewmaster!</h1>;
+    }
+}
+
+
