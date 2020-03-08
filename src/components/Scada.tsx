50d4d5989673a826546115a8c22d6be5fe4b4d07
Mmmattt123 <matej.janeba@gmail.com>
2020-03-08 16:03:35 +0100

    added rotuer

---
+++ src/components/Scada.tsx
@@ -0,0 +1,21 @@
+import React = require('react');
+import {Line} from 'react-chartjs-2';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import {makeStyles, Typography} from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import orange from "@material-ui/core/colors/orange";
+
+
+export default function Scada(){
+//@ts-ignore
+
+	//const classes = useStyles();
+	return (
+		<div>
+			<Typography variant={"h2"}> SCADA</Typography>
+		</div>
+	
+	)
+}
