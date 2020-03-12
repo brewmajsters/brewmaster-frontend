50d9ff4b514372ac4cc7f89927933599d81c3f53
Mmmattt123 <matej.janeba@gmail.com>
2020-03-12 18:38:04 +0100

    addition of basic HMI

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,52 +7,76 @@ import amber from '@material-ui/core/colors/amber';
 import ThemeProvider from '@material-ui/styles/ThemeProvider' ;
 import Dashboard from "./Dashboard";
 import red from "@material-ui/core/colors/red";
+import {
+	BrowserRouter ,
+	Switch,
+	Route,
+	Link
+} from "react-router-dom";
+import Scada from "./Scada";
 
 const drawerWidth = 240;
 
 const BrewTheme = createMuiTheme({
-    palette: {
-        primary: amber,
-    },
-    overrides: {
-        MuiCard: {
-            root: {
-                borderRadius: 0,
-                boxShadow: '0px 0px 0px 2px rgba(158, 158, 158,1)',
-            },
-        },
-    }
+	palette: {
+		primary: amber,
+	},
+	overrides: {
+		MuiCard: {
+			root: {
+				borderRadius: 0,
+				boxShadow: '0px 0px 0px 2px rgba(158, 158, 158,1)',
+			},
+		},
+	}
 });
 
 const useStyles = makeStyles(theme => ({
-    body: {
-        display: 'flex',
-    },
-    main:{
-        marginTop: 100,
+	body: {
+		display: 'flex',
+	},
+	main:{
+		marginTop: 100,
 
-        [theme.breakpoints.up('sm')]: {
-            width: `calc(100% - ${drawerWidth}px)`,
-        },
-    }
+		[theme.breakpoints.up('sm')]: {
+			width: `calc(100% - ${drawerWidth}px)`,
+		},
+	}
 
 }));
 
 
 
 export default function App (){
-    // @ts-ignore
-    const classes = useStyles();
+	// @ts-ignore
+	const classes = useStyles();
 
-        return (
-            <ThemeProvider theme={BrewTheme}>
-                 <div className={classes.body}>
-                        {NavBars()}
-                        <div className={classes.main}>
-                            {Dashboard()}
-                        </div>
-                 </div>
-            </ThemeProvider>
-        );
+	return (
+
+	<BrowserRouter>
+		<ThemeProvider theme={BrewTheme}>
+			<div className={classes.body}>
+				{NavBars()}
+
+				<div className={classes.main}>
+
+					<Switch>
+						<div>
+							<Route exact path="/"> 
+							
+							<Scada />
+						</Route>
+						<Route path="/dashboard/"> 
+
+							<Dashboard />
+						</Route>
+					</div>
+					</Switch>
+				</div>
+
+			</div>
+		</ThemeProvider>
+	</BrowserRouter>
+);
 
 }
--- src/components/Scada.tsx
+++ src/components/Scada.tsx
@@ -6,16 +6,121 @@ import {makeStyles, Typography} from "@material-ui/core";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import orange from "@material-ui/core/colors/orange";
+import InputBase from '@material-ui/core/InputBase';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+
+const useStyles = makeStyles(theme => ({
+	container: {
+		width: '100%',
+	},
+	motorBlack: {
+		position: 'relative',
+		top: '-145px',
+		left: '229px',
+		color: 'white',
+		minWidth: '326px',
+		minHeight: '142px',
+		maxWidth: '326px',
+		maxHeight: '142px',
+		backgroundImage: 'url("/public/motor_black.png")'
+	},
+	motorRPM: {
+		color: 'white', 
+		position: 'relative', 
+		top: '71px',
+		left: '200px',
+		fontFamily: 'Roboto',
+	}, 
+	boilerBlack: {
+		position: 'relative',
+		left: '34px',
+		minWidth: '171px',
+		minHeight: '210px',
+		maxWidth: '171px',
+		maxHeight: '210px',
+		backgroundImage: 'url("/public/boiler_black.png")'
+	},
+	boilerRPM: { 
+
+	} 
+	transmission: {
+		position: 'relative',
+		left: '0rem',
+		minWidth: '228px',
+		minHeight: '172px',
+		maxWidth: '228px',
+		maxHeight: '172px',
+		backgroundImage: 'url("/public/Transmission.png")'
+	},
+	root: {
+		padding: '2px 4px',
+		display: 'flex',
+		alignItems: 'center',
+		width: 400,
+	},
+	input: {
+		marginLeft: theme.spacing(1),
+		flex: 1,
+	},
+	submitButton: {
+		padding: 10,
+	},
+
+}));
+
+function ScadaImage(){
+	const classes = useStyles();
+	return (
+		<div className={classes.container} >
+			<div className={classes.boilerBlack}> 
+
+			</div>
+			<div className={classes.transmission}>
+			</div> 
+			<div className={classes.motorBlack}> 
+				<div className={classes.motorRPM}> 
+					100 rpm
+				</div>
+			</div>
+			<div > 
+				<Paper component="form" className={classes.root}>
+					<InputBase
+		className={classes.input}
+		placeholder="Set RPM per engine"
+		inputProps={{ 'aria-label': 'set rpm for engine' }}
+		/>
+		<IconButton type="submit" className={classes.submitButton} aria-label="search">
+		Submit! 
+	</IconButton>
+</Paper>
+			</div> 
+
+
+		</div>
+
+
+	)
+
+}
 
 
 export default function Scada(){
-//@ts-ignore
+	//@ts-ignore
 
-	//const classes = useStyles();
+	const classes = useStyles();
 	return (
-		<div>
-			<Typography variant={"h2"}> SCADA</Typography>
-		</div>
-	
-	)
+
+	<div>
+		<Grid container spacing={10}>
+			<Grid item> 
+				<Typography variant={"h2"}> SCADA</Typography>
+			</Grid>
+			<Grid item xs={10} >
+				<ScadaImage />
+			</Grid> 
+		</Grid>
+	</div>
+
+)
 }
