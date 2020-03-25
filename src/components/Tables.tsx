57fc1393d68271e5fb8fd30fe43ad9ce2b634ed6
Mmmattt123 <matej.janeba@gmail.com>
2020-03-25 19:55:07 +0100

    generic modification

--- src/components/Tables.tsx
+++ src/components/Tables.tsx
@@ -11,60 +11,119 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import {JsonToTable} from "react-json-to-table";
+//import TableContainer from '@material-ui/core/TableContainer';
+//import {JsonToTable} from "react-json-to-table";
 
 const classes = getClasses();
 
-export class Tables extends Component {
-getTable(link: string) {
-    return (                   
-        fetch(link)
-            .then(response => response.json())
-            .then((jsonData) => {
-                // jsonData is parsed json object received from url
-                return (
-                    <div className="App">
-                      <JsonToTable json={jsonData} />
-                    </div>
-                  );
-            })
-            .catch((error) => {
-            // handle your errors here
-            console.log("Unable to get data")
-        })  
-    );
-}
+const headersDatapoints = ["name", "units","datapoint code", "legend","writable"]
 
-ButtonNavigate = function (props: {name: string}) {
+const headersProtocols = ["name","protocol"]
+
+ export function tableHeaders(headerLabels: string[]){
     return (
-        <BottomNavigation
-        //value={value}
-         onChange={(event, newValue) => {
-             this.state = this.getTable(newValue);
-         }}
-         showLabels
-        
-
-     //className={classes.test}
-     >
- <BottomNavigationAction label="Datapoints" value='https://virtserver.swaggerhub.com/Brewmaster/Brewmaster/1.0.0/datapoints' icon={<FolderIcon />} />
- <BottomNavigationAction label="Protocol" value='https://virtserver.swaggerhub.com/Brewmaster/Brewmaster/1.0.0/protocols' icon={<FolderIcon />} />
- <BottomNavigationAction label="Device" value='https://virtserver.swaggerhub.com/Brewmaster/Brewmaster/1.0.0/devices' icon={<FolderIcon />} />
- <BottomNavigationAction label="Modules" value='https://virtserver.swaggerhub.com/Brewmaster/Brewmaster/1.0.0/modules' icon={<FolderIcon />} />
- <BottomNavigationAction label="Device Type" value='https://virtserver.swaggerhub.com/Brewmaster/Brewmaster/1.0.0/devicetypes' icon={<FolderIcon />} />
-
-     </BottomNavigation>
+        <TableHead>
+            <TableRow>
+            {headerLabels.map((label) =>
+                <TableCell>
+                    {label}
+                </TableCell>
+            )}
+            </TableRow>
+        </TableHead>
     );
-
 }
-// todo: tuto mozes robit svoje tabulky, state moze mat iba
+
+interface DatapointsData {
+    "name": string,
+    "units": string,
+}
+
+export function tableDatapoints(data: any){
+    return(
+        <TableBody>
+            {data.map( row =>
+                <TableRow>
+                    {row.map(value =>
+                    <TableCell>
+                        {value}                        
+                    </TableCell>
+                    )}
+                </TableRow>
+            )}
+        </TableBody>
+    );
+}
+
+export class Tables extends Component<{},{datapointsData: any, datapointsIsLoaded: boolean}> {
+
+//    state = {
+//        active: 1
+//    }
+
+    constructor(props){
+        super(props);
+        this.setState({
+            datapointsIsLoaded: false
+        })
+    }
+
+    getTable = (link: string) => {
+        return (
+            fetch(link)
+                .then(response => response.json())
+                .then((jsonData) => {
+                    // jsonData is parsed json object received from url
+                    this.setState({
+                        datapointsData: jsonData,
+                        datapointsIsLoaded: true
+                    });
+                    console.log(this.state)
+                })
+                .catch((error) => {
+                    // handle your errors here
+                    console.log("Unable to get data")
+                })
+        );
+    }
+
+    ButtonNavigate = (props: { name: string }) =>  {
+        return (
+            <div>
+            <BottomNavigation
+                //value={value}
+                onChange={(event, newValue) => {
+                    this.getTable(newValue)
+                }}
+                showLabels
+            >
+                <BottomNavigationAction label="Datapoints" value='https://virtserver.swaggerhub.com/Brewmaster/Brewmaster/1.0.0/datapoints' icon={<FolderIcon />} />
+                <BottomNavigationAction label="Protocol" value='https://virtserver.swaggerhub.com/Brewmaster/Brewmaster/1.0.0/protocols' icon={<FolderIcon />} />
+                <BottomNavigationAction label="Device" value='https://virtserver.swaggerhub.com/Brewmaster/Brewmaster/1.0.0/devices' icon={<FolderIcon />} />
+                <BottomNavigationAction label="Modules" value='https://virtserver.swaggerhub.com/Brewmaster/Brewmaster/1.0.0/modules' icon={<FolderIcon />} />
+                <BottomNavigationAction label="Device Type" value='https://virtserver.swaggerhub.com/Brewmaster/Brewmaster/1.0.0/devicetypes' icon={<FolderIcon />} />
+
+                </BottomNavigation>
+                                </div>
+        );
+
+    }
+    // todo: tuto mozes robit svoje tabulky, state moze mat iba
     render() {
         return (
             <div>
-            <this.ButtonNavigate name={"Buttons"} />
-             {this.state} </div>
+                <this.ButtonNavigate name={"Buttons"} />
+                {this.state}
+                    <Table>
+                        {tableHeaders(headersDatapoints)}
+                    </Table>
+                     <Table>
+                        {tableHeaders(headersProtocols)}
+                    </Table>
+
+                 </div>
         );
-       
+
     }
 };
 
