0190170cfea4df63eeb69e536e5bf122f7cec5e7
Vtijana <tijanavereski@gmail.com>
2020-03-25 17:09:56 +0100

    pridanie tabuliek na jednotlive moduly- pokus 1

--- src/components/Tables.tsx
+++ src/components/Tables.tsx
@@ -1,17 +1,71 @@
 import React = require("react");
-import { Component } from "react"; 
+import { Component } from "react";
 import getClasses from "../styles/tables_style";
+import Button from '@material-ui/core/Button';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import FolderIcon from '@material-ui/icons/Folder';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import {JsonToTable} from "react-json-to-table";
 
 const classes = getClasses();
 
-export class Tables extends Component { 
-// todo: tuto mozes robit svoje tabulky, state moze mat iba
+export class Tables extends Component {
+getTable(link: string) {
+    return (                   
+        fetch(link)
+            .then(response => response.json())
+            .then((jsonData) => {
+                // jsonData is parsed json object received from url
+                return (
+                    <div className="App">
+                      <JsonToTable json={jsonData} />
+                    </div>
+                  );
+            })
+            .catch((error) => {
+            // handle your errors here
+            console.log("Unable to get data")
+        })  
+    );
+}
 
+ButtonNavigate = function (props: {name: string}) {
+    return (
+        <BottomNavigation
+        //value={value}
+         onChange={(event, newValue) => {
+             this.state = this.getTable(newValue);
+         }}
+         showLabels
+        
+
+     //className={classes.test}
+     >
+ <BottomNavigationAction label="Datapoints" value='https://virtserver.swaggerhub.com/Brewmaster/Brewmaster/1.0.0/datapoints' icon={<FolderIcon />} />
+ <BottomNavigationAction label="Protocol" value='https://virtserver.swaggerhub.com/Brewmaster/Brewmaster/1.0.0/protocols' icon={<FolderIcon />} />
+ <BottomNavigationAction label="Device" value='https://virtserver.swaggerhub.com/Brewmaster/Brewmaster/1.0.0/devices' icon={<FolderIcon />} />
+ <BottomNavigationAction label="Modules" value='https://virtserver.swaggerhub.com/Brewmaster/Brewmaster/1.0.0/modules' icon={<FolderIcon />} />
+ <BottomNavigationAction label="Device Type" value='https://virtserver.swaggerhub.com/Brewmaster/Brewmaster/1.0.0/devicetypes' icon={<FolderIcon />} />
+
+     </BottomNavigation>
+    );
+
+}
+// todo: tuto mozes robit svoje tabulky, state moze mat iba
     render() {
         return (
-            <h1 style={classes.itWorks}> It Wokrs! </h1>
+            <div>
+            <this.ButtonNavigate name={"Buttons"} />
+             {this.state} </div>
         );
+       
     }
-}
+};
 
 export default Tables;
--- src/styles/tables_style.tsx
+++ src/styles/tables_style.tsx
@@ -6,6 +6,9 @@ export default function getClasses(){
     return ({
         itWorks:{
          color: 'red'
+        },
+        test : {
+            padding:50
         }
         //sem mozes pridavat svoje custom CSS/JSS pre tabulky vo formate JSON 
     });
