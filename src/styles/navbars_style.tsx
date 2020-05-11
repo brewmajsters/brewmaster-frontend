ece6510e498fd9b4e0eb637cf8229a1ac4cfd8cd
Mmmattt123 <matej.janeba@gmail.com>
2020-05-11 15:28:46 +0200

    Revert "Filterable senzors"

--- src/styles/navbars_style.tsx
+++ src/styles/navbars_style.tsx
@@ -18,21 +18,13 @@ export default function getClasses() {
                 padding: 10,
             },
             drawerPaper: {
-                marginTop: 25,
+                marginTop: 50,
+                width: 300,
+                padding: 10,
+                zIndex: 1,
             },
             content: {
                 flexGrow: '1',
-            },
-            gridList: {
-                width: '170px',
-                marginTop: '50px',
-                marginLeft: '20px',
-                textAlign: 'center' as 'center',
-                align: 'center' as 'center'
-            },
-            leftBarTile: {
-                width: '170px',
-
             }
             //toolbar: theme.mixins.toolbar,
         });
